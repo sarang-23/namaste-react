f1a99b9a284f7761fbc595ad988fb4e84f655237
sarang-23 <saranggupta25@gmail.com>
2023-12-17 15:44:50 +0530

    Episode 2 learnings

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "Nested Heading"),
@@ -17,6 +19,4 @@ const heading = React.createElement(
   "Hello World from React!"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 root.render(parent);
-root2.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Namaste React
+#Namaste React
+
+#Parcel for packaging
+
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching algorithms - written in c++
+- Cache - for faster builds
+- Image Optimizations
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential bundling - supports older browser
+- Diagnostics
+- Error handling
+- SSL support
+- Tree shaking
