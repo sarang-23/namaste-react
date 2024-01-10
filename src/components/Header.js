b1a079c9bcf3c4450541dda4a476a7a3838904df
Sarang Gupta <saranggupta25@gmail.com>
2024-01-10 22:53:00 +0530

    Custom hook for online status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { LOGO_URL, CART_IMAGE } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const onlineStatus = useOnlineStatus();
+
   return (
     <div className="header">
       <img src={LOGO_URL} alt="Logo" />
       <div className="nav-links">
         <ul>
+          <li>Online Status : {onlineStatus ? "online" : "offline"}</li>
           <li>
             <Link to="/"> Home </Link>
           </li>
